60f4ab4cd1783f7ff56634d074b22028f2fa9aa5
Hambal-cmd <labeebhambal@gmail.com>
2025-10-07 19:17:16 +0530

    Third try is the charm

---
+++ Python/Basic-Calc.py
@@ -0,0 +1,6 @@
+# This was testing for how to make sub forlder. Most basic project that comes to mind is a calculator.
+
+a = int(input("Enter the first value: "))
+b = int(input("Enter the second value: "))
+
+print("Sum of the give numbers is: ",(a+b))
